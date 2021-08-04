a47db7f1dbd1510fa7e80f35f7ea3df9aeca3594
João Costa <costav1402@gmail.com>
2021-08-04 11:09:59 -0300

    consuming api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import './App.css';
 import CurrencyRow from './Components/CurrencyRow';
-
+const apiKey = process.env.REACT_APP_API_KEY;
+const url =`https://free.currconv.com/api/v7/convert?q=USD_BRL&compact=ultra&apiKey=${apiKey}`
 function App() {
+  useEffect(()=>{
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=> console.log(data))
+  },[])
   return (
     <Fragment>
       <h1>Convert</h1>
